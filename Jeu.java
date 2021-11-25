491887f339b840d0347bcba3fb2d973029ac9a42
enzx95 <enzo.grz95@gmail.com>
2021-11-25 21:38:26 +0100

    Added Print and Fill board functions

--- Jeu.java
+++ Jeu.java
@@ -22,6 +22,7 @@ public class Jeu {
             switch (saisieMenu) {
             case 1:
                 System.out.println("Playing");
+                fillBoard();
                 break;
             case 2:
                 System.exit(0);
@@ -41,16 +42,63 @@ public class Jeu {
         return i;
     }
 
-    public static void initializeboard() {
-        Playerturns = 0;
-        numTotal = 0;
-        gamestate = true;
-        numTurns = 0;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; i < 8; i++) {
-               // if()
+    // public static void initializeboard() {
+    // Playerturns = 0;
+    // numTotal = 0;
+    // gamestate = true;
+    // numTurns = 0;
+    // for (int i = 0; i < 8; i++) {
+    // for (int j = 0; i < 8; i++) {
+    // // if()
+    // }
+    // }
+    // }
+
+    private static void fillBoard() {
+
+        for (int i = 0; i < 8; ++i)
+            for (int j = 0; j < 8; ++j)
+                board[i][j] = ' ';
+        for (int i = 0; i < 3; ++i) {
+            if (i % 2 == 0) {
+                for (int j = 1; j < 8; j += 2)
+                    board[i][j] = 'a';
+            } else {
+                for (int j = 0; j < 8; j += 2)
+                    board[i][j] = 'a';
             }
         }
+        for (int i = 7; i > 4; --i) {
+            if (i % 2 == 0) {
+                for (int j = 1; j < 8; j += 2)
+                    board[i][j] = 'b';
+            } else {
+                for (int j = 0; j < 8; j += 2)
+                    board[i][j] = 'b';
+            }
+        }
+        printBoard();
+    }
+
+    private static void printBoard() {
+        for (int i = 1; i < 9; ++i) {
+            System.out.print("   " + i);
+        }
+        System.out.print("\n  ");
+        for (int i = 0; i < 8; ++i)
+            System.out.print("+---");
+        System.out.print("+\n");
+        for (int i = 0; i < 8; ++i) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < 8; ++j) {
+                System.out.print("| " + board[i][j] + " ");
+            }
+            System.out.print("|\n  ");
+            for (int i2 = 0; i2 < 8; ++i2)
+                System.out.print("+---");
+            System.out.print("+\n");
+        }
+
     }
 
 }
